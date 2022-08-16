7a308a10c5ab684041b3a7e2adad3e9e1717ddf1
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-16 14:57:50 +0300

    Anguular App API Test

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from './Book';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  url = 'http://localhost:8000/book';
+  book: Book[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getBooks() {
+    return this.http.get(this.url);
+  }
+
+  // getBook(id) {
+  //   return this.http.get(this.url + '/' + id);
+  // }
+
+  // createBook(book) {
+  //   return this.http.post(this.url, book);
+  // }
+  // updateBook(book) {
+  //   return this.http.put(this.url + '/' + book.id, book);
+  // }
+  // deleteBook(id) {
+  //   return this.http.delete(this.url + '/' + id);
+  // }
+
+  // getBookByName(name) {
+  //   return this.http.get(this.url + '/' + name);
+  // }
 }
